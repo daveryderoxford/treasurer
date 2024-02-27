65697381d697ebc866a58953c7507b0a6d6f5b9c
daveryderoxford <dave@theryderclan.co.uk>
2024-02-27 11:33:41 +0000

    Display claims for selected user.

--- src/app/expense-claim/claim.service.ts
+++ src/app/expense-claim/claim.service.ts
@@ -1,70 +1,67 @@
 import { Injectable, Signal, effect, signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { Auth } from '@angular/fire/auth';
-import { CollectionReference, Firestore, collection, collectionData, deleteDoc, doc, getCountFromServer, setDoc } from '@angular/fire/firestore';
+import { CollectionReference, Firestore, collection, collectionData, deleteDoc, doc, getCountFromServer, getDocs, query, setDoc, where } from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
-import { shareReplay, tap } from 'rxjs/operators';
+import { shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
 import { Claim } from './claim.model';
 import { AuthService } from '../auth/auth.service';
 
 const CLAIMS_COLLECTION = 'claims'
 
-@Injectable( {
-    providedIn: 'root'
-  } )
-  export class ClaimService {
-  
-    private claims$ = of<Claim[]>([]);
-    claims = signal<Claim[]>([]);
-  
-    constructor ( private fs: Firestore,
-      private auth: AuthService ) {
+@Injectable({
+  providedIn: 'root'
+})
+export class ClaimService {
 
-      effect( () {
-        if (auth.isTreasurer()) {
-          const claimCollection = collection( this.fs, CLAIMS_COLLECTION ) as CollectionReference<Claim>;
+  public claims: Signal<Claim[]>;
 
-          this.claims$ = collectionData<Claim>( claimCollection ).pipe(
-            tap( claims => console.log('Claim count: ' + claims.length)),
-            shareReplay( 1 ),
-          );
-          this.claims = toSignal(this.claims$, { initialValue: [] });
+  constructor(private fs: Firestore,
+    private auth: AuthService) {
+
+    const claimCollection = collection(this.fs, CLAIMS_COLLECTION) as CollectionReference<Claim>;
+
+    const claims$ = toObservable(this.auth.user).pipe(
+      switchMap((user) => {
+        if (!user) {
+          return of<Claim[]>([])
         } else {
-          
-
+          const q = this.auth.isTreasurer() ?
+            query(claimCollection) :
+            query(claimCollection, where('userId', '==', user.uid));
+          return collectionData(q);
         }
       })
-  
+    );
 
+    this.claims = toSignal(claims$, {initialValue: []});
 
+  }
 
-    }
-  
-    async update( id: string, claim: Partial<Claim> ): Promise<void> {
-      const d = doc( this.fs, CLAIMS_COLLECTION, id );
-      await setDoc( d, claim, { merge: true } );
-    }
+  async update(id: string, claim: Partial<Claim>): Promise<void> {
+    const d = doc(this.fs, CLAIMS_COLLECTION, id);
+    await setDoc(d, claim, { merge: true });
+  }
 
-    private async getCount(): Promise<number> {
+  private async getCount(): Promise<number> {
     const coll = collection(this.fs, CLAIMS_COLLECTION);
     const snapshot = await getCountFromServer(coll);
     return snapshot.data().count;
   }
-  
-    async add( claim: Partial<Claim> ): Promise<void> {
 
-      // Generate next numeric id
-      const id = await this.getCount()+1;
-      claim.id = 'EX-' + id.toString();
+  async add(claim: Partial<Claim>): Promise<void> {
 
-      await setDoc(doc(this.fs, CLAIMS_COLLECTION, claim.id), claim);
-      
-    }
-  
-    async delete( id: string ): Promise<void> {
-      const d = doc( this.fs, CLAIMS_COLLECTION, id );
-      await deleteDoc( d );
-    }
+    // Generate next numeric id
+    const id = await this.getCount() + 1;
+    claim.id = 'EX-' + id.toString();
+
+    await setDoc(doc(this.fs, CLAIMS_COLLECTION, claim.id), claim);
 
   }
-  
+
+  async delete(id: string): Promise<void> {
+    const d = doc(this.fs, CLAIMS_COLLECTION, id);
+    await deleteDoc(d);
+  }
+
+}
